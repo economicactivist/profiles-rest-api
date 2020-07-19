7df1eefadfa213ce4446dda5c9e56dd4f30720ad
Mountain Scott <mountainscott67@yahoo.com>
2020-07-19 07:57:00 -0400

    added helloapiview

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+# the above should be removed for creating custom api
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class HelloApiView(APIView):
+    """Test API View"""
+    # allows us to define the application logic for the endpoint we'll assign to this view
+    # it chooses the right function based on the request
+    # a URL will be assigned to the API
+
+    def get(self, request, format=None):
+        """Returns a list of APIView features"""
+        # 'format' won't be used but it's best practice to include it
+        an_apiview = [
+            'Uses HTTP methods as functions (get, post, patch, put, delete)',
+            'Is similar to a traditional Django View but built specifically for APIs',
+            'Gives you the most control over application logic',
+            'Is mapped manually to URLs',
+        ]
+
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
 
 # Create your views here.
--- profiles_project/urls.py
+++ profiles_project/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls), #links admin url to admin app (in the UI)
+    path('api/', include('profiles_api.urls'))
 ]
