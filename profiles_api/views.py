5d531c001b1a3909f8e97e82fac1c0f000e35faf
economicactivist <mountainscott67@yahoo.com>
2020-07-20 08:45:47 -0400

    added login API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -5,8 +5,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
-from rest_framework.authentication import TokenAuthentication
+from rest_framework.authentication import TokenAuthentication #prevent users form modifying other user info
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken #for logging in
+from rest_framework.settings import api_settings
+
 # list of useful http status codes that you can use when returning responses from your api
 from profiles_api import serializers
 from profiles_api import models
@@ -128,3 +131,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     # permissions are used for fine-grained authorization
     filter_backends = (filters.SearchFilter, )
     search_fields = ('name', 'email', )
+
+class UserLoginApiView(ObtainAuthToken):
+    '''Handle creating user authentication tokens'''
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    #views(ets) have this by default 
